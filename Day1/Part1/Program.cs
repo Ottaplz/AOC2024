4699d25683a2bec3bc2244fabefeb5e377cad189
David Murray <dcmurray13@hotmail.com>
2025-01-02 18:19:14 +1030

    The two parts of Day 1 completed

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿// The program must take two separate lists of values and compare the differences between the sorted lists
+// e.g. the difference between the smallest number of each list, second smallest number of each list etc..
+// The program must then print the total sum of differences to answer the challenge.
+
+// My thought process completing this challenge was to create two lists for the left and right columns and then sort them
+// Once sorted finding the difference between paired values is easy to sum
+
+// Declare both lists
+List<int> listL = new List<int>();
+List<int> listR = new List<int>();
+int totalDiff = 0;
+string[] inputs;
+string input;
+
+// Receieve input from file containing challenge values
+StreamReader reader = new StreamReader("C:\\values.txt");
+input = reader.ReadLine();
+
+// Separate each readline into two different int values
+while (input != null) 
+{
+    inputs = input.Split("   ");
+    listL.Add(int.Parse(inputs[0]));
+    listR.Add(int.Parse(inputs[1]));
+    input = reader.ReadLine();
+}
+
+reader.Close();
+
+// Sort lists
+listL.Sort();
+listR.Sort();
+
+// Compare each ordered pair for diference in value and tally total difference amongst all pairs
+for (int i = 0; i < listL.Count; i++)
+{
+    totalDiff += Math.Abs(listL[i] - listR[i]);
+}
+
+// Print Total
+Console.WriteLine(totalDiff);
---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,54 @@
+﻿// The program must take values from two lists and then determine their "similarity score"
+// The similarity score is determined by identifying if a value in the left list of values 
+// exists in the right list of values.
+// If the value exists in the right list of values then the 
+
+// My approach for this problem was to create a hash table of the right list of values with a key value pair of value and frequency
+// After creating the hash table the left list can then be compared and added to the similarity score for key * value
+
+
+// Declare necessary variables and data structures
+List<int> listL = new List<int>();
+Dictionary<int, int> DictR = new Dictionary<int, int>();
+int totalSimilarity = 0;
+string[] inputs;
+string input;
+
+StreamReader reader = new StreamReader("C:\\values.txt");
+input = reader.ReadLine();
+
+// Receive input from file and split the values
+while (input != null) 
+{
+    inputs = input.Split("   ");
+    int rightVal = int.Parse(inputs[1]);
+
+    // Turn the left input into a list
+    listL.Add(int.Parse(inputs[0]));
+
+    // Turn the right input into a dictionary<int, int> with the first value being the number input and the second value being number of times it occurs
+    if (DictR.ContainsKey(rightVal))
+    {
+        DictR[rightVal]++;
+    }
+    else
+    {
+        DictR.Add(rightVal, 1);
+    }
+
+    input = reader.ReadLine();
+}
+
+reader.Close();
+
+// Parse the left list and check the values against the Dictionary keys, if exists multiply by number of occurences and add to total
+foreach (int num in listL)
+{
+    if (DictR.TryGetValue(num, out int value))
+    {
+        totalSimilarity += num * value;
+    }
+}
+
+// Print the total tallies value
+Console.WriteLine(totalSimilarity);
